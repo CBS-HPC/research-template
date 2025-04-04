c22bee43dde7de94a5097dfe65a71ba12122d273
kgp <kgp.lib@cbs.dk>
2025-04-04 12:39:12 +0200

    sdf

--- {{cookiecutter.repo_name}}/setup/project_setup.py
+++ {{cookiecutter.repo_name}}/setup/project_setup.py
@@ -344,7 +344,7 @@ license = "{{cookiecutter.open_source_license}}"
 repo_name = "{{cookiecutter.repo_name}}"
 version_control = "{{cookiecutter.version_control}}"
 programming_language = "{{cookiecutter.programming_language}}"
-remote_backup = "{{cookiecutter.remote_backup}}"
+remote_backup = "{{cookiecutter.rclone_backup}}"
 
 programming_language, authors, orcids = correct_format(programming_language, authors, orcids)
 programming_language, python_env_manager,r_env_manager,code_repo, remote_storage, install_cmd, conda_r_version, conda_python_version  = set_options(programming_language,version_control)
