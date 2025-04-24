874838e6b0ae7b626ac678a0bf8ce7cb452c331a
kgp <kgp.lib@cbs.dk>
2025-04-24 17:30:32 +0200

    sfsf

--- {{cookiecutter.repo_name}}/setup/utils/readme_templates.py
+++ {{cookiecutter.repo_name}}/setup/utils/readme_templates.py
@@ -80,8 +80,8 @@ def creating_readme(repo_name= None, repo_user = None ,project_name=None, projec
             setup += (f"**Software Re-installation using {py_version}**\n"
                 f"The function below re-installs all ./setup and ./src software dependencies. The script should be executed using **{py_version}**\n")
         else:
-            setup += {f"**Software Re-installation using Conda**\n"
-                "The function below re-installs all ./setup and ./src software dependencies.\n"}
+            setup += (f"**Software Re-installation using Conda**\n"
+                "The function below re-installs all ./setup and ./src software dependencies.\n")
         
         setup += (
                 "```\n"
