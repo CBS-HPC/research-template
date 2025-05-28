b658decde88802f5b3dbe0aeac1067d4d6154c36
kgp <kgp.lib@cbs.dk>
2025-05-28 16:32:09 +0200

    pip install uv

--- {{cookiecutter.repo_name}}/setup/intro.py
+++ {{cookiecutter.repo_name}}/setup/intro.py
@@ -33,6 +33,7 @@ def main():
     orcids = load_from_env("ORCIDS",".cookiecutter")
 
     # Install packages
+    subprocess.run([sys.executable, "-m", "pip", "install", "python-dotenv"], check=True)
     pip_packages = set_packages(version_control,programming_language)
     package_installer(required_libraries = pip_packages)
     print(f'Packages {pip_packages} installed successfully in the current environment.')
