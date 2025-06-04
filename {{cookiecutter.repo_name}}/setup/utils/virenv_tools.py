ab0aac300665177a296272a4651888ef7709e156
kgp <kgp.lib@cbs.dk>
2025-06-04 15:31:44 +0200

    ci readme

--- {{cookiecutter.repo_name}}/setup/utils/virenv_tools.py
+++ {{cookiecutter.repo_name}}/setup/utils/virenv_tools.py
@@ -427,6 +427,7 @@ def create_requirements_txt(requirements_file:str="requirements.txt"):
     # Get the Python executable path from sys.executable
     python_executable = sys.executable
     
+    print(python_executable)
     # Use subprocess to run pip freeze and capture the output
     result = subprocess.run([python_executable, "-m", "pip", "freeze"], capture_output=True, text=True)
     
