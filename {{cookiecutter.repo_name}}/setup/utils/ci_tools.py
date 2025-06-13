94668479ed12528fe2a2404fcbcfaf4204fd9a59
kgp <kgp.lib@cbs.dk>
2025-06-13 13:11:25 +0200

    j2 install

--- {{cookiecutter.repo_name}}/setup/utils/ci_tools.py
+++ {{cookiecutter.repo_name}}/setup/utils/ci_tools.py
@@ -36,7 +36,7 @@ def generate_ci_configs(programming_language, code_repo, project_root="."):
     code_repo = code_repo.lower()
     version = parse_version(get_version(programming_language), programming_language)
 
-    path_jinja_templates("j2_templates/ci_templates")
+    patch_jinja_templates("j2_templates/ci_templates")
     template_env = set_jinja_templates("j2_templates/ci_templates")
 
     file_map = {
