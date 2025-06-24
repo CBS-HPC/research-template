58666b5d00bea96ba7b79214c4830f8be2f74224
kgp <kgp.lib@cbs.dk>
2025-06-24 10:53:12 +0200

    sadad

--- {{cookiecutter.repo_name}}/setup/utils/general_tools.py
+++ {{cookiecutter.repo_name}}/setup/utils/general_tools.py
@@ -48,8 +48,8 @@ def install_uv():
         try:
             print("Installing 'uv' package into current Python environment...")
             subprocess.run(
-                #[sys.executable, "-m", "pip", "install", "uv"],
-                [sys.executable, "-m", "pip", "install", "--upgrade", "uv"],
+                #[sys.executable, "-m", "pip", "install", "--upgrade", "uv"],
+                [sys.executable, "-m", "pip", "install", "uv"],
                 check=True,
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL,
