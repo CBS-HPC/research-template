0c848693f1a566d819057c1e0adb0b50ddd25be6
kgp <kgp.lib@cbs.dk>
2025-06-25 11:33:40 +0200

    sf

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,17 @@
 import subprocess
 import pathlib
 import sys
+import platform
+
+
+def run_in_venv():
+    if platform.system() == "Windows":
+        cmd = r".venv\Scripts\activate.bat && python setup\project_setup.py"
+        subprocess.run(cmd, shell=True)  # Uses cmd.exe by default
+    else:
+        cmd = "source .venv/bin/activate && python setup/project_setup.py"
+        subprocess.run(cmd, shell=True, executable="/bin/bash")  # Uses bash
+
 
 def install_uv():
     """Ensure 'uv' is installed and usable."""
@@ -11,28 +22,29 @@ def install_uv():
     except (subprocess.CalledProcessError, FileNotFoundError):
         print("⚠️ 'uv' not found. Attempting to install it via pip...")
         try:
-            subprocess.run(["pip", "install", "--upgrade", "uv"],check=True)
+            subprocess.run([sys.executable, "pip", "install", "--upgrade", "uv"],check=True)
             #subprocess.run([sys.executable, "-m", "pip", "install", "uv"], check=True)
             # Verify installation
             subprocess.run(["uv", "--version"], check=True, stdout=subprocess.DEVNULL)
             return True
         except subprocess.CalledProcessError:
             return False
-def create_venv_with_uv(env_path):
+
+def create_with_uv():
     """Create virtual environment using uv."""
     subprocess.run(["uv", "init"], check=True)
     subprocess.run(["uv", "venv"], check=True)
     subprocess.run(["uv", "lock"], check=True)
-    return env_path / ("Scripts" if sys.platform.startswith("win") else "bin") / "python"
-
+    subprocess.run(["uv", "add","uv"], check=True)
+    
 def main():
     env_path = pathlib.Path(".venv")  # Now it's a Path object
 
     # Create venv only if it doesn't exist
     if not env_path.exists() and install_uv():
             try:
-                python_path = create_venv_with_uv(env_path)
-                subprocess.run([str(python_path), "setup/project_setup.py"])
+                create_with_uv()
+                run_in_venv()
             except subprocess.CalledProcessError as e:
                 subprocess.run(["python", "setup/project_setup.py"])
     else:
