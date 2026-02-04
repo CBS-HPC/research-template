5282d06420316c8d5368fba6435f25f41d73f903
kgp <kgp.libas@cbs.dk>
2026-02-04 10:49:56 +0100

    Fix Path import in tomlutils

--- {{cookiecutter.repo_name}}/setup/repokit/common/tomlutils.py
+++ {{cookiecutter.repo_name}}/setup/repokit/common/tomlutils.py
@@ -2,7 +2,7 @@ import json
 import os
 import sys
 import pathspec
-from  pathspec import Path
+from pathlib import Path
 from pathspec.patterns import GitWildMatchPattern
 from typing import Optional, Union
 
