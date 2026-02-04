a702b702ffa74e19cf9976d955ce02bb97554f16
kgp <kgp.libas@cbs.dk>
2026-02-04 11:48:30 +0100

    Ensure hooks run from project root

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -272,6 +272,8 @@ def create_with_pip():
 
 
 def main():
+    project_root = pathlib.Path(__file__).resolve().parent.parent
+    os.chdir(project_root)
     env_path = pathlib.Path(".venv")
     if not env_path.exists():
         if install_uv():
